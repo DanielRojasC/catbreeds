87490792ba66b77d83ba4a88a797682ff6b5cb04
Daniel Eduardo Rojas Cortés <daniel.rojas@sunsetswh.com>
2025-02-22 11:27:03 -0500

    feat: :lipstick: added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,19 @@ import 'package:catbreeds/app/utils/constants/path_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
-  Future.value(WidgetsFlutterBinding.ensureInitialized());
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await dotenv.load(fileName: PathConstants.environmentPath);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
+    statusBarColor: Colors.white,
+    statusBarBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
   ));
+
   runApp(const CatBreedsApp());
+  FlutterNativeSplash.remove();
 }
